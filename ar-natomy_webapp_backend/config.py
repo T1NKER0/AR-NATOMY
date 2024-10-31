4564e492eef7702f125a2a4e6506c972006e74f2
MARCOS <101586428+T1NKER0@users.noreply.github.com>
2024-10-31 12:49:29 -0800

    Delete ar-natomy_webapp_backend directory

--- ar-natomy_webapp_backend/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    HOST = '127.0.0.1'
-    DATABASE = 'ar-natomy'
-    USER = 'root'
-    PASSWORD = 'Marcos0206'
--- ar-natomy_webapp_backend/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Create a blueprint for user-related routes
-user_routes = Blueprint('user_routes', __name__)
-
-from .user_routes import *  # Import user routes
-
